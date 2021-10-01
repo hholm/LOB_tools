7d4d5072cca3f57face5d211e87c83df630e4988
Henry Holm <32178231+hholm@users.noreply.github.com>
2021-10-01 17:20:17 -0400

    Minor refactoring, starting some vignettes for later publication

--- R/LOB_plotdata.R
+++ R/LOB_plotdata.R
@@ -26,7 +26,7 @@ LOB_plotdata <- function(peakdata,plot_type,choose_class = NULL) {
   # plot RTF data
   if (plot_type %in% "RTF") {
     
-    if(is.null(LOBpeaklist$Flag_RF)){"No RTF screened data to plot."}
+    if(is.null(LOBpeaklist$Flag_RF)){stop("No RTF screened data columns in object.")}
     
     # subset to classes based on inputs
     if (is.null(choose_class)) {
@@ -41,7 +41,7 @@ LOB_plotdata <- function(peakdata,plot_type,choose_class = NULL) {
     for (i in 1:length(lipid_classes)) {
       #insert a class check
       print(ggplot(data = subset(LOBpeaklist, species == lipid_classes[i] & degree_oxidation == 0)) +
-              geom_point(aes(x = peakgroup_rt, y = LOBdbase_mz, color = Flag), size = 3) +
+              geom_point(aes(x = peakgroup_rt, y = LOBdbase_mz, color = Flag_RF), size = 3) +
               scale_color_manual(values = c("Red" = "#e55934", "ms2v" = "#fdbd4c", "5%_rtv" = "#9bc53d", "10%_rtv" = "#0f5f20", "Double_Peak?" = "#5bc0eb", "Unknown" = "#ff99ff")) +
               geom_point(aes(x = DBase_std_RF * Std_rt_RF, y = LOBdbase_mz, color = Flag), shape = 3) +
               geom_errorbarh(aes(xmax = DBase_std_RF * Std_rt_RF * 1.1, xmin = DBase_std_RF * Std_rt_RF * 0.9, height = 0.2, y = LOBdbase_mz, color = Flag)) +
